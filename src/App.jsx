ee5acd196b2cee6b76f9b09aad457e53a49eb523
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2022-01-06 19:24:55 -0500

    add search function for find the weather information about a specific city or country

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import WeatherInfo from './components/WeatherInfo';
+
 function App() {
   const [data, setData] = useState({});
-  const [query, setQuery] = useState('london');
+  const [query, setQuery] = useState('');
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${API_KEY}`;
 
-  const fetchData = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-        console.log(result);
-      });
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      fetch(url)
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className="container">
-      <main className="weather-container">
-        <input type="text" className="search-box" placeholder="Search" />
+      <main
+        className={
+          typeof data.main != 'undefined'
+            ? data.main.temp > 16
+              ? 'weather-container warm-bg'
+              : 'weather-container'
+            : 'weather-container'
+        }>
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
         <div>
           {typeof data.main != 'undefined' ? (
             <WeatherInfo
